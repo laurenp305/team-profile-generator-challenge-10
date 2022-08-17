52b1ad58dcd319dda47421fd5533a2843836d08d
laurenp305 <lamor1800@gmail.com>
2022-08-17 16:31:54 -0400

    added employee.test

---
+++ tests/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require('./Employee');
+const newEmployee = new Employee('Jared', '1', 'jared@fakemail.com');
+
+test('This is a test to see if we can get the value for the employee object', () => {
+    expect(newEmployee.getName()).toBe('Jared');
+    expect(newEmployee.getId()).toBe('1');
+    expect(newEmployee.getEmail()).toBe('jared@fakemail.com');
+});
+
+test('Test to see if we can get the name from getName()', () => {
+    expect(newEmployee.getName()).toBe('Jared');
+});
+
+test('Test to see if we can get the id from getId()', () => {
+    expect(newEmployee.getId()).toBe('1');
+});
+
+test('Test to see if we can get the email from getEmail()', () => {
+    expect(newEmployee.getEmail()).toBe('jared@fakemail.com');
+} );
+
+test('Test to see if we can get employees role from getRole') , () => {
+    expect(newEmployee.getRole()).toBe('Employee');
+}
+
+
