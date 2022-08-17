da6276f786245cb54450e234acec6dcdd601c64a
laurenp305 <lamor1800@gmail.com>
2022-08-17 17:19:39 -0400

    corrected employee const in employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require('./Employee');
+const Employee = require('.lib/Employee');
 const newEmployee = new Employee('Jared', '1', 'jared@fakemail.com');
 
 test('This is a test to see if we can get the value for the employee object', () => {
