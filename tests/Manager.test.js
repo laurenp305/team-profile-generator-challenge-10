56d4f7903fc3e0a862eaf3bf9e914c5eb1e8370f
laurenp305 <lamor1800@gmail.com>
2022-08-18 19:39:15 -0400

    updated manager test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,32 +3,14 @@ const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
 
 //DID THIS THE SAME WAY I RAN THE EMPLOYEE & INTERN TESTS
 test('Test if we can get values for manager objects', () => {
-    expect(newManager.getName()).toBe('Jared');
-    expect(newManager.getId()).toBe('1');
-    expect(newManager.getEmail()).toBe('jared@fakemail.com');
-    expect(newManager.getOfficeNumber()).toBe('1');
+    const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
+
+    expect(newManager.officeNumber()).toEqual(expect.any(Number));
 } );
 
-test('This is a test to see if we can get the value for the manager object', () => {
-    expect(newManager.getName()).toBe('Jared');
-    expect(newManager.getId()).toBe('5');
-    expect(newManager.getEmail()).toBe('jared@fakemail');
-    expect(newManager.getOfficeNumber()).toBe('1');
-} );
+test('Test to see if we can get the role'), () => {
 
-test('Test to see if we can get the name from getName()', () => {
-    expect(newManager.getName()).toBe('Jared');
-});
+    const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
 
-test('Test to see if we can get the id from getId()', () => {
-    expect(newManager.getId()).toBe('1');
-});
-
-test('Test to see if we can get the email from getEmail()', () => {
-    expect(newManager.getEmail()).toBe('jared@fakemail.com');
-} );
-
-test('Test to see if we can get employees role from getRole') , () => {
-    expect(newManager.getOfficeNumber()).toBe('1');
+    expect(newManager.getRole()).toEqual(expect.stringContaining('Manager'));
 }
-
