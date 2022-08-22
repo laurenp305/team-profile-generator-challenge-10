be293b088b25b5ba6d99bd0c422d25e2c82ecbc8
laurenp305 <lamor1800@gmail.com>
2022-08-22 18:41:36 -0400

    fixed syntax issues with manager test js

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -8,9 +8,9 @@ test('Test if we can get values for manager objects', () => {
     expect(newManager.officeNumber()).toEqual(expect.any(Number));
 } )
 
-test('Test to see if we can get the role'), () => {
+test('Test to see if we can get the role', () => {
 
     const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
 
     expect(newManager.getRole()).toEqual(expect.stringContaining('Manager'));
-}
+})
