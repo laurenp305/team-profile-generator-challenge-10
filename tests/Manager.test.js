1aba31c9f4cb38751bccef8a131d788b6b3e3d5f
laurenp305 <lamor1800@gmail.com>
2022-08-23 16:33:14 -0400

    fixed test for manager constructor values

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,29 +1,12 @@
 const Manager = require('../lib/Manager');
 
-//Office number test for manager
-test("Set office number via constructor", () => {
-    const testNumber = 1;
+const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
 
-    const newManager = new Manager('Jared', 1,'jared@fakemail.com', 1);
-
-    expect(newManager.officeNumber).toBe(testNumber)
-})
-
-//Test to see if we could get the role of the employee
-test("Test to see if we can get the role", () => {
-    const testRole = "Manager";
-
-    const newManager = new Manager('Jared', 1,'jared@fakemail.com', 1);
-
-    expect(newManager.getRole()).toBe(testRole);
-})
-
-//Test to see if the office number actually populates
-test("Get office number", () => {
-    const testNumber = 1;
-
-    const newManager = new Manager('Jared', 1,'jared@fakemail.com', 1);
-
-    expect(newManager.getOfficeNumber()).toBe(testNumber);
-})
+//DID THIS THE SAME WAY I RAN THE EMPLOYEE & INTERN TESTS
+test('Test if we can get values for manager objects', () => {
+    expect(newManager.name).toBe('Jared');
+    expect(newManager.id).toBe('1');
+    expect(newManager.email).toBe('jared@fakemail.com');
+    expect(newManager.officeNumber).toBe('1');
+} )
 
