b926b06f53b15d06ddba492e4c6bf8f63f70b018
laurenp305 <lamor1800@gmail.com>
2022-08-23 16:21:31 -0400

    changed test to get manager role

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,16 +2,20 @@ const Manager = require('../lib/Manager');
 const newManager = new Manager('Jared', '1','jared@fakemail.com', '1');
 
 //Office number test for manager
-test("Set office number via constructor"), () => {
+test("Set office number via constructor", () => {
     const testNumber = 1;
+
     const newManager = new Manager('Jared', 1,'jared@fakemail.com', 1);
+
     expect(newManager.officeNumber).toBe(testNumber)
-}
+})
 
-test('Test to see if we can get the role', () => {
+test("Test to see if we can get the role", () => {
+    const testRole = "Manager";
 
     const newManager = new Manager('Jared', 1,'jared@fakemail.com', 1);
 
-    expect(newManager.getRole()).toEqual(expect.stringContaining("Manager"));
+    expect(newManager.getRole()).toBe(testRole);
 })
+
 
