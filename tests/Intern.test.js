20a6b894451285c666b40b25774ea6fd8dafe59a
laurenp305 <lamor1800@gmail.com>
2022-08-23 16:50:03 -0400

    fixed test values for intern objects

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,11 +1,14 @@
 const Intern = require('../lib/Intern');
 
+const newIntern = new Intern('John', '5','john@fakemail.com', '2University');
+
 //DID THIS THE SAME WAY I RAN THE EMPLOYEE TEST
 test('Tests if we can get values for intern objects', () => {
 
-    const newIntern = new Intern('John', '5','john@fakemail.com', '2University');
-
-    expect(newIntern.getName()).toEqual(expect.any(String));
+    expect(newIntern.name).toBe('John');
+    expect(newIntern.id).toBe('5');
+    expect(newIntern.email).toBe('john@fakemail.com');
+    expect(newIntern.school).toBe('2University');
 
 } )
 
