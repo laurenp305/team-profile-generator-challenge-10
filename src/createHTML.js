20e137a87eadeb50bec9844d05ef162bd80821f9
laurenp305 <lamor1800@gmail.com>
2022-08-25 17:55:11 -0400

    added create manager and engineer cards

--- src/createHTML.js
+++ src/createHTML.js
@@ -15,3 +15,21 @@ const createManager = (manager) => {
 </div>`
 }
 
+//creates Engineer section/card
+const createEngineer = (engineer) => {
+    return `<div class="card">
+    <div class="card-header">
+        <h2>${engineer.name}</h2>
+        <h3>${engineer.title}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: ${engineer.email}</li>
+            <li class="list-group-item">Github: ${engineer.github}</li>
+        </ul>
+    </div>
+</div>`
+}
+
+
