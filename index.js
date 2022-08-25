95559d60cab0ddb10122674d8540b539dfae40d4
laurenp305 <lamor1800@gmail.com>
2022-08-25 18:20:47 -0400

    created function with prompts to add new manager

--- index.js
+++ index.js
@@ -7,3 +7,68 @@ const Engineer = require("./lib/Engineer");
 
 const team = [];
 
+// start of manager prompts 
+const addNewManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the manager of this team?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter a name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+            validate: idInput => {
+                if  (isNaN(idInput)) {
+                    console.log ("Please enter an ID!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email address?",
+            validate: emailInput => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email address!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office number?",
+            validate: officeInput => {
+                if  (isNaN(officeInput)) {
+                    console.log ('Please enter an office number!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const  { name, id, email, officeNumber } = managerInput; 
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArray.push(manager); 
+        console.log(manager); 
+    })
+};
